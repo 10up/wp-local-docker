e2126457eeb7030a6f211b0142465dbe23b125c1
Christian Chung <christian.m.chung@gmail.com>
2017-05-16 16:25:37 -0400

    Add bin scripts to help quickstart elasticpress

---
+++ bin/elasticpress.bat
@@ -0,0 +1,5 @@
+@echo off
+
+docker-compose exec --user www-data phpfpm wp plugin install elasticpress --activate
+docker-compose exec --user www-data phpfpm wp option set ep_host "http://docker-local.dev:$(docker-compose port elasticsearch 9200 | cut -f 2 -d ':')"
+docker-compose exec --user www-data phpfpm wp elasticpress index
