e2e0cd19f31cc7567e75b0012c9dad0c8cbc8ce2
Christian Chung <christian.m.chung@gmail.com>
2017-05-16 18:14:40 -0400

    Update elasticsearch internal host

--- bin/elasticpress.bat
+++ bin/elasticpress.bat
@@ -2,6 +2,6 @@
 
 docker-compose exec --user www-data phpfpm wp plugin install elasticpress --activate
 FOR /F "tokens=1,2 delims=/" %a IN ("docker-compose port elasticsearch 9200") DO set ip=%a&set port=%b
-set "host=http://docker-local.dev:%port%"
+set "host=http://elasticsearch:%port%"
 docker-compose exec --user www-data phpfpm wp option set ep_host "%host%"
 docker-compose exec --user www-data phpfpm wp elasticpress index
