95e438e6ed68fb2c139ae6fbeb64eb8642391c8a
Christian Chung <christian.m.chung@gmail.com>
2017-05-16 17:00:49 -0400

    Replace cut in elasticpress batch script with dos compat substr

--- bin/elasticpress.bat
+++ bin/elasticpress.bat
@@ -1,5 +1,7 @@
 @echo off
 
 docker-compose exec --user www-data phpfpm wp plugin install elasticpress --activate
-docker-compose exec --user www-data phpfpm wp option set ep_host "http://docker-local.dev:$(docker-compose port elasticsearch 9200 | cut -f 2 -d ':')"
+FOR /F "tokens=1,2 delims=/" %a IN ("docker-compose port elasticsearch 9200") DO set ip=%a&set port=%b
+set "host=http://docker-local.dev:%port%"
+docker-compose exec --user www-data phpfpm wp option set ep_host "%host%"
 docker-compose exec --user www-data phpfpm wp elasticpress index
