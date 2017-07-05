07ff0e2afa23f3cdb3669c73b3710cb6d979c3ba
Ricardo Moraleida <moraleida@gmail.com>
2017-07-05 08:18:35 -0300

    determine the user regardless of where the script is called from

--- bin/wp_docker_user.sh
+++ bin/wp_docker_user.sh
@@ -3,5 +3,19 @@
 # prints the username defined on docker-compose.override.yml
 # used on wp.sh and setup.sh to define the container user
 wp_docker_user() {
-    echo "$(grep WP_DOCKER_USER docker-compose.override.yml | tr -d '[:space:]' | tr -d 'WP_DOCKER_USER:')"
+    # move into the root folder
+    called_path=$('pwd')
+    parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P )
+    cd "$parent_path"
+    cd ..
+
+    # read user from file
+    if [ -f docker-compose.override.yml ]; then
+        echo "$(grep WP_DOCKER_USER docker-compose.override.yml | tr -d '[:space:]' | tr -d 'WP_DOCKER_USER:')"
+    else
+        echo ""
+    fi;
+
+    # move back where called
+    cd "$called_path"
 }
