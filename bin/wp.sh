5fe5ba23b020a4b5011cc0027ac04ad623201dee
Ricardo Moraleida <moraleida@gmail.com>
2017-07-05 08:23:30 -0300

    move logic around for better DRYness

--- bin/wp.sh
+++ bin/wp.sh
@@ -4,9 +4,4 @@
 source "$(dirname "$0")/wp_docker_user.sh";
 WP_DOCKER_USER="$(wp_docker_user)";
 
-# If the information is invalid or non-existent, fallback to the www-data user
-if [ -z $WP_DOCKER_USER ]; then
-  WP_DOCKER_USER="www-data"
-fi;
-
 docker-compose exec --user $WP_DOCKER_USER:www-data phpfpm wp "$@"
--- bin/wp_docker_user.sh
+++ bin/wp_docker_user.sh
@@ -11,10 +11,17 @@ wp_docker_user() {
 
     # read user from file
     if [ -f docker-compose.override.yml ]; then
-        echo "$(grep WP_DOCKER_USER docker-compose.override.yml | tr -d '[:space:]' | tr -d 'WP_DOCKER_USER:')"
+        WP_DOCKER_USER="$(grep WP_DOCKER_USER docker-compose.override.yml | tr -d '[:space:]' | tr -d 'WP_DOCKER_USER:')"
+
+        # If the information is invalid or non-existent, fallback to the www-data user
+        if [ -z $WP_DOCKER_USER ]; then
+          WP_DOCKER_USER="www-data"
+        fi;
     else
-        echo ""
+        WP_DOCKER_USER="www-data"
     fi;
+
+    echo $WP_DOCKER_USER
 
     # move back where called
     cd "$called_path"
